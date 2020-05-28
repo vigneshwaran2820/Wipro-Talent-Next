027f5333690cba7b5fd13208afab1b55230582c9
vigneshwaran2820 <60315497+vigneshwaran2820@users.noreply.github.com>
2020-05-28 22:08:12 +0530

    Create returnlast.java

---
+++ Mettl/Day4/Returnlastdigit/returnlast.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import  java.util.*;
+class usermaincode
+{
+  public int addlastdigits(int intput1,int input2)
+  {
+   if(n<0)
+		n *= -1;
+		if(n<9)
+		return -1;
+		else 
+		{
+			n=(n%100)/10;
+			return n;
+		}
+    }
+    }
