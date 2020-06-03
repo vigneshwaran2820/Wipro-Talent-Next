83926cd4091c6b8bfbe7483a10ea8b8f497bdd98
vigneshwaran2820 <60315497+vigneshwaran2820@users.noreply.github.com>
2020-06-03 19:39:54 +0530

    Create Day10_2.java

---
+++ HackerRank/Day10_2.java
@@ -0,0 +1,14 @@
+class Result {
+public static int minOperations(List<Integer> ratings) {
+Collections.sort(ratings);  
+     int sum=0;
+    int n=ratings.size();
+    for(int i=1;i<n;i+=2)
+    {
+        sum+= ratings.get(i)-ratings.get(i-1);
+        
+    }
+    return sum;
+    }
+   
+}
