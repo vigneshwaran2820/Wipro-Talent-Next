74af1880f8470ee4146ee50698b926fb9fba8aae
vigneshwaran2820 <60315497+vigneshwaran2820@users.noreply.github.com>
2020-06-03 19:54:46 +0530

    Update Day7_1.java

--- HackerRank/Day7_1.java
+++ HackerRank/Day7_1.java
@@ -1,29 +1,22 @@
-public static List<Integer> counts(List<Integer> teamA, List<Integer> teamB) {
-    // Write your code here
-    int count=0,sum=0,z;
-    int sz=teamB.size();
-    int sx=teamA.size();   
-    List<Integer> teamC=new teamC<Integer>();  
-    int sum1;  
-    for(int i=0;i<sx;i++)
-    {
-        
-    
-            teamB.get(i);
-            teamA.get(i);
-            for(int j=0;j<sz;j++)
-            {
-             if(teamA(i)<=teamB(j))
-            {
-                sum1+=1;               
+char[] v = { 'a', 'e', 'i', 'o', 'u' };
+        HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+        char c;
+        int vCount = -1;
 
-            }   
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            int value = charCount.get(c) == null ? 0 : charCount.get(c) + 1;
+            if (value == 0) {
+
+                if (c == v[vCount + 1]) {
+                    value = vCount >= 0 ? charCount.get(v[vCount]) + 1 : 1;
+                    vCount++;
+                }
+                
+                charCount.put(c, value);
+
+            } else {
+                charCount.put(c, value);
             }
-            teamC.add(sum);
-            sum-=sum;
-            
-            
-       }
-       return teamC;
-    }
- }
+        }
+        return charCount.get('u').intValue();
