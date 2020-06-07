e94958752f3d9821afefdc80d0fc3361dcefbe2e
vigneshwaran2820 <60315497+vigneshwaran2820@users.noreply.github.com>
2020-06-07 22:15:46 +0530

    Create sumofodd_and_even.java

---
+++ Mettl/Day8/Sum_of_odd_even/sumofodd_and_even.java
@@ -0,0 +1,29 @@
+nt temp=0,even=0,temp1=0,odd=0;
+		String s1=new String ("odd");
+		String s2=new String ("even");
+		int c=input1;
+		
+		
+			while(input1>0)
+			{
+				temp=input1%10;
+				if(temp%2==0)
+				{
+					even+=temp;
+				}
+				else
+				{
+					odd+=temp;
+				}
+				input1/=10;
+			}
+			if(s1.equals(input2))
+			return odd;
+			else
+			return even;
+		
+		
+		
+			
+	}
+}
