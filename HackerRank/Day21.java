8247d1de4f1d184996a37323ebcef5e95b656bc9
vigneshwaran2820 <60315497+vigneshwaran2820@users.noreply.github.com>
2020-06-16 12:02:38 +0530

    Create Day21.java

---
+++ HackerRank/Day21.java
@@ -0,0 +1,12 @@
+public static void printList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
